9185556d47ff9a4acb5ceeae3c12b4c5511e02e3
gustavosiqueira94 <gustavo.siquira94@gmail.com>
2016-09-02 12:42:17 -0300

    Version 2.2 minor improvements on storage and logging

--- src/main/oai_generic_crawler.py
+++ src/main/oai_generic_crawler.py
@@ -12,11 +12,12 @@ from time import time, ctime
 from xml.etree.ElementTree import Element
 from xml.etree.ElementTree import tostring
 import logging
-
+import HTMLParser
+import re
 
 __author__ = 'Gustavo Siqueira'
 
-
+FILE_ESCAPE_CHARS = r'\]\[\/\\\;\,\>\<\&\*\:\%\=\+\@\!\#\^\(\)\|\?\^'
 METADATA = 'oai_dc'
 ENCODE = 'utf-8'
 SEPARATOR = '================================'
@@ -26,23 +27,25 @@ class OAICrawler():
     '''
         This class provides a basic OAI crawler for repositories that use such a protocol.
     '''
+
     def __init__(self):
-        logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)s) -   %(message)s')
+        logging.basicConfig(filename='./crawler.log',
+                            filemod='w',
+                            level=logging.DEBUG,
+                            format='[%(levelname)s] (%(threadName)s) -   %(message)s')
         self.logger = logging.getLogger(__name__)
         self.unvisited_repository = list()
 
-
     def pool_worker(self, repository_list, threads=4):
         start_time = time()
-        self.logger.info('Startinf process at {0}.'.format(ctime()))
+        self.logger.info('Starting process at {0}.'.format(ctime()))
 
         pool = Pool(threads)
-        pool.map(self.retrieval, repository_list)
+        pool.imap_unordered(self.retrieval, repository_list)
         pool.close()
         pool.join()
 
         self.logger.info('Process done at {0}. Total time: {1} seconds'.format(ctime(), time() - start_time))
-
 
     def retrieval(self, repository):
         self.logger.info('Trying to retrieve url {0}'.format(repository[1]).encode(ENCODE))
@@ -59,6 +62,8 @@ class OAICrawler():
             # identify info
             identify = client.identify()
             repository_name = identify.repositoryName()
+            repository_name_normalized = re.sub(re.compile(FILE_ESCAPE_CHARS), '', repository_name).strip() \
+                .replace(' ', '_').lower()
             base_url = identify.baseURL().encode(ENCODE)
             protocol_version = identify.protocolVersion().encode(ENCODE)
             granularity = identify.granularity().encode(ENCODE)
@@ -67,53 +72,56 @@ class OAICrawler():
 
             metadata = {'repository_name': repository_name,
                         'base_url': base_url,
+                        'latest_url': repository[1],
                         'protocol_version': protocol_version,
                         'granularity': granularity,
                         'compression': str(compression).strip('[]'),
                         'deleted_record': deleted_record}
 
             self.logger.info('Repository name: {0}'.format(repository_name))
+            self.logger.info('URL connected: {0}'.format(repository[1]))
             self.logger.info('Base URL: {0}'.format(base_url))
             self.logger.info('Protocol version: {0}'.format(protocol_version))
             self.logger.info('Granularity: {0}'.format(granularity))
             self.logger.info('Compression: {0}'.format(compression))
             self.logger.info('Deleted record: {0}'.format(deleted_record))
 
-            if not exists(''.join(['files/', repository[0], '/'])):
-                self.logger.info('Creating storage folder for {0}...'.format(repository_name))
-                makedirs(''.join(['files/', repository[0], '/']))
+            records_count = 0
+            deleted_count = 0
+            records_list = list()
+            parsed_records_list = list()
 
-            self.logger.info('Creating metadata file...')
-            meta_file = open(''.join(['files/', repository[0], '/metadata.xml']), 'w')
+            # we're not interested in all sets, so we must iterate over the ones we have and want to crawl
+            if repository[2] is not None:
+                self.logger.info('Fetching set {0}...'.format(repository[2]))
+                records_list = client.listRecords(metadataPrefix=METADATA, set=repository[2])
+            else:
+                records_list = client.listRecords(metadataPrefix=METADATA)
+            if records_list is not None:
+                for record in records_list:
+                    records_count += 1
+                    if record[0].isDeleted():
+                        deleted_count += 1
+                    if record[1] is not None:
+                        parsed_records_list.append(tostring(record[1].element()))
+                self.logger.info(
+                    'Retrieved {0} records from set {1} where {2} were deleted'.format(records_count, repository[2],
+                                                                                       deleted_count))
+            if not exists(''.join(['files/', repository_name_normalized, '/'])):
+                self.logger.info('Creating storage folder for {0}...'.format(repository_name))
+                makedirs(''.join(['files/', repository_name_normalized, '/']))
+
+            self.logger.info('Creating storage files...')
+            meta_file = open(''.join(['files/', repository_name_normalized, '/metadata.xml']), 'w')
+            metadata['records_number'] = records_count
+            metadata['deleted_number'] = deleted_count
             meta_file.write(tostring(dict_to_xml('metadata', metadata)))
             meta_file.close()
 
-            record_count = 0
-            deleted_count = 0
-            records = list()
-
-            # we're not interested in all sets, so we must iterate over the ones we want to crawl
-            if repository[2] != 'None':
-                self.logger.info('Fetching set {0}...'.format(repository[2]))
-                for record in client.listRecords(metadataPrefix=METADATA, set=repository[2]):
-                    record_count += 1
-                    if record[0].isDeleted():
-                        deleted_count += 1
-                    records.append(tostring(record[1].element()))
-                self.logger.info(
-                    'Retrieved {0} records from set {1} where {2} were deleted'.format(record_count, repository[2],
-                                                                                       deleted_count))
-            else:
-                for record in client.listRecords(metadataPrefix=METADATA, set=repository[2]):
-                    record_count += 1
-                    if record[0].isDeleted():
-                        deleted_count += 1
-                    records.append(tostring(record[1].element()))
-                self.logger.info(
-                    'Retrieved {0} records from set {1} where {2} were deleted'.format(record_count, repository[2],
-                                                                                       deleted_count))
-            record_file = open(''.join(['files/', repository[0], '/', repository[0], '.xml']), 'w')
-            record_file.write(''.join(records))
+            record_file = open(''.join(
+                ['files/', repository_name_normalized, '/', repository_name_normalized, '_', repository[2], '.xml']),
+                'w')
+            record_file.write(''.join(parsed_records_list))
             record_file.close()
 
         except NoRecordsMatchError, nrme:
@@ -123,8 +131,8 @@ class OAICrawler():
             if nrme.message == 'No matches for the query':
                 self.unvisited_repository.append(repository)
 
-        # if any unexpected error occurs, we must keep tracking
         except Exception, e:
+            # if any unexpected error occurs, we must keep tracking
             self.logger.error(e.message, exc_info=True)
 
 
@@ -148,16 +156,27 @@ def parse_repository_file(file):
     with open(file, 'r') as rep_file:
         for line in rep_file:
             line_components = line.strip().split('\t')
+
+            # parsing database names with encoded characters
+            line_components[0] = HTMLParser.HTMLParser().unescape(line_components[0])
+
+            # parsing sets
             if line_components[2] != 'None':
                 line_components[2] = line_components[2].split('=')[1]
+            # else:
+            #    line_components[2] = None
+
+            # parsing metadata_prefix
             line_components[3] = line_components[3].split('=')[1]
+
             parsed_repository_list.append(line_components)
     return parsed_repository_list
 
 
 if __name__ == '__main__':
     # Main usage
-    OAICrawler().pool_worker(parse_repository_file('world_repositories'))
+    # OAICrawler().pool_worker(parse_repository_file('world_repositories'))
 
     # Test usage
-    #OAICrawler().pool_worker([['AUT University Doctoral Theses','http://aut.researchgateway.ac.nz/dspace-oai/request','col_10292_4','mets']])
+    OAICrawler().pool_worker([['AUT University Doctoral Theses', 'http://aut.researchgateway.ac.nz/dspace-oai/request',
+                               'col_10292_4', 'mets']])
